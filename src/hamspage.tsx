2eb37f94d75566b002ab883e2b8a7a17b5c09bc3
ceecosmonew <pagefamex@gmail.com>
2024-09-02 22:40:29 +0100

    Add files via upload

--- src/hamspage.tsx
+++ src/hamspage.tsx
@@ -1,2 +1,18 @@
-  
+import React from 'react'
 
+function hamspage() {
+  return (
+    <div>
+    <h4 className=''>This is HamsPay official Page with Airdrop</h4>
+    <h5 className=''>Copy the link and send to your friends: you will be rewarded with $5,000 for each referral</h5>
+  <div className="butt">
+    <a href="https://t.me/share/url?url=https://t.me/HamsPay_bot" target="_blank">Share on Telegram</a>
+    <a href="https://twitter.com/intent/tweet/url?url=https://t.me/HamsPay_bot" target="_blank">Share on Twitter</a>
+      <a href="https://api.whatsapp.com/send?text=https://t.me/HamsPay_bot" target="_blank">Share on Whatsapp</a>
+    
+    </div>
+    </div>
+  )
+}
+
+export default hamspage
