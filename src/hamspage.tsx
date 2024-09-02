56e100e978fd6720d3222320e2daf887adc8484c
ceecosmonew <pagefamex@gmail.com>
2024-09-02 22:53:22 +0100

    Update hamspage.tsx

--- src/hamspage.tsx
+++ src/hamspage.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function hamspage() {
   return (
     <div>
-    <h4 className=''>This is HamsPay official Page with Airdrop</h4>
-    <h5 className=''>Copy the link and send to your friends: you will be rewarded with $5,000 for each referral</h5>
+    <h4 className="">This is HamsPay official Page with Airdrop</h4>
+    <h5 className="">Copy the link and send to your friends: you will be rewarded with $5,000 for each referral</h5>
   <div className="butt">
     <a href="https://t.me/share/url?url=https://t.me/HamsPay_bot" target="_blank">Share on Telegram</a>
     <a href="https://twitter.com/intent/tweet/url?url=https://t.me/HamsPay_bot" target="_blank">Share on Twitter</a>
