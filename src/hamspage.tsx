fe187d116df0aaad09d107f5e2c51671be68cea9
ceecosmonew <pagefamex@gmail.com>
2024-09-02 22:13:52 +0100

    Update hamspage.tsx

--- src/hamspage.tsx
+++ src/hamspage.tsx
@@ -3,8 +3,8 @@
 
 <div className="butt">
   <a href="https://t.me/share/url?url=https://t.me/HamsPay_bot" target="_blank">Share on Telegram</a>
-  <a href="https://twitter.com/intent/tweet/url?url=https://t.me/HamsPay_bot" target="_blank">Share on Twitter</a></div>
-  <a href="https://api.whatsapp.com/send?text=https://t.me/HamsPay_bot" target="_blank">Share on Whatsapp</a>
+  <a href="https://twitter.com/intent/tweet/url?url=https://t.me/HamsPay_bot" target="_blank">Share on Twitter</a>
+    <a href="https://api.whatsapp.com/send?text=https://t.me/HamsPay_bot" target="_blank">Share on Whatsapp</a>
   
   </div>
 
