c9c0112416bd841132f6f880ce08aaad443f0bdb
ceecosmonew <pagefamex@gmail.com>
2024-09-02 21:17:36 +0100

    Update hamspage.tsx

--- src/hamspage.tsx
+++ src/hamspage.tsx
@@ -1,1 +1,10 @@
+  This is HamsPay official Page with Airdrop
+  Copy the link and send to your friends: you will be rewarded with $5,000 for each referral.
+
+<div className="butt">
+  <a href="https://t.me/share/url?url=https://t.me/HamsPay_bot" target="_blank">Share on Telegram</a>
+  <a href="https://twitter.com/intent/tweet/url?url=https://t.me/HamsPay_bot" target="_blank">Share on Twitter</a></div>
+  <a href="https://api.whatsapp.com/send?text=https://t.me/HamsPay_bot" target="_blank">Share on Whatsapp</a>
+  
+  </div>
 
